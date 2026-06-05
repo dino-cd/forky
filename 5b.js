697dfca112849bd9cf76cde9b2843d5237caff54
dino-cd <nulldino1@gmail.com>
2026-06-05 08:51:24 +0800

    hah..

--- 5b.js
+++ 5b.js
@@ -10896,11 +10896,19 @@ let delta;
 function rAF60fps() {
 const slider = document.getElementById('ssslider');
 const sslider = document.getElementById('sssval');
+const sweight = document.getElementById('ssweight');
+const ssweight = document.getElementById('weightval');
 slider.addEventListener('input', function() {
     allxspeed = parseFloat(this.value);
     sslider.textContent = this.value;
     for (let i = 0; i < charD.length; i++) {
         charD[i][4] = allxspeed;
+    }
+sweight.addEventListener('input', function() {
+    allxsweight = parseFloat(this.value);
+    ssweight.textContent = this.value;
+    for (let i = 0; i < charD.length; i++) {
+        charD[i][2] = allxsweight
     }
 });
 	requestAnimationFrame(rAF60fps);
